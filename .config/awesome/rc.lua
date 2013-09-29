84a436ae2d6d629ddb37a7e8f85846b4586be335
David Schlyter <dschlyter@gmail.com>
2013-09-29 23:43:45 +0200

    awesome: set terminal to urxvt

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -40,8 +40,8 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
-editor = os.getenv("EDITOR") or "editor"
+terminal = "urxvt"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
