8d7d2c529f300a7101863271cb9ce9ee5db7cf36
David Schlyter <dschlyter@gmail.com>
2017-07-29 18:13:13 +0200

    hammerspoon: fix alt-tab

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -49,10 +49,10 @@ end)
 -- alt tab between individual windows
 -------------------------------------
 
-switcher_space = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{}, 
+switcher_space = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{},
     {showTitles = false, showSelectedThumbnail = false})
-hs.hotkey.bind('alt','tab','Next window',function()switcher_space:next()end)
-hs.hotkey.bind('alt-shift','tab','Prev window',function()switcher_space:previous()end)
+hs.hotkey.bind({'alt'},'tab',nil,function()switcher_space:next()end)
+hs.hotkey.bind({'alt', 'shift'},'tab',nil,function()switcher_space:previous()end)
 
 -- reimplements focusWindowX with less buggy and more powerful functionality
 -- first try to find the best window, then fallback to all windows in that direction
