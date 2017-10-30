9b9935c1e2701b716213b3e281ec437ff17b0940
David Schlyter <dschlyter@gmail.com>
2017-10-30 17:10:25 +0100

    reimplement preq script in python

--- script_template.py
+++ script_template.py
@@ -1,11 +1,16 @@
-#/usr/bin/python
+#!/usr/bin/python
 
+import argparse
 import subprocess
 import sys
-import argparse
+import re
+import json
+from pprint import pprint
+
+desciption='TODO describe script'
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='TODO describe script')
+    parser = argparse.ArgumentParser(description=desciption)
     parser.add_argument('-c', '--counter', dest='counter', default=14, type=int,
             help='a simple counter')
     parser.add_argument('-f', '--flag', dest='flag', action='store_true',
@@ -18,24 +23,37 @@ def parse_args():
 
 def main():
     args = parse_args()
-    print run("ls -la "+args.first)
-    print run("sleep 10")
+    print run("ls -la '"+args.first+"'")
 
 def run(command, shell=False, canFail=False, output=False):
     try:
-        if isinstance(command, str):
-            if shell:
-                command = ["bash", "-c", command]
-            else:
-                command = command.split(" ")
-        stdout = subprocess.check_output(command)
+        parsed = parse_command(command, shell)
+        stdout = subprocess.check_output(parsed)
         if output:
             print stdout
-        return stdout
+        return stdout.strip()
     except Exception as e:
         if canFail:
             return None
         else:
             raise
+
+def parse_command(command, shell):
+    if isinstance(command, str):
+        if re.search("['\"|]", command):
+            shell = True
+        if shell:
+            return ["bash", "-c", command]
+        return command.split(" ")
+    return command
+
+def load_conf(path):
+    with open(path, 'r') as data_file:
+        return json.load(data_file)
+
+def save_conf(path, data):
+    with open(path, 'w') as data_file:
+        print json.dumps(data)
+        json.dump(data, data_file)
 
 main()
