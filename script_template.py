28e1cac06c64837e1f6970aea0c81dd17690c17e
David Schlyter <dschlyter@gmail.com>
2017-11-16 15:55:09 +0100

    update script template

--- script_template.py
+++ script_template.py
@@ -6,11 +6,12 @@ import sys
 import re
 import json
 from pprint import pprint
+from tempfile import TemporaryFile
 
-desciption='TODO describe script'
+description='TODO describe script'
 
 def parse_args():
-    parser = argparse.ArgumentParser(description=desciption)
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-c', '--counter', dest='counter', default=14, type=int,
             help='a simple counter')
     parser.add_argument('-f', '--flag', dest='flag', action='store_true',
@@ -25,25 +26,42 @@ def main():
     args = parse_args()
     print run("ls -la '"+args.first+"'")
 
-def run(command, shell=False, canFail=False, output=False):
-    try:
-        parsed = parse_command(command, shell)
-        if not output:
+
+# high level fire and forget execution
+def run(command, shell=False, can_fail=False, output=False):
+    parsed = parse_command(command, shell)
+    if not output:
+        try:
             return subprocess.check_output(parsed).strip()
-        else:
-            ret_val = subprocess.call(parsed)
-            if ret_val != 0 and not canFail:
-                raise Exception("command returned "+str(ret_val))
-            return ret_val == 0
-    except Exception as e:
-        if canFail:
-            return None
-        else:
-            raise
+        except subprocess.CalledProcessError as e:
+            if can_fail:
+                return None
+            else:
+                raise
+    else:
+        ret_val = subprocess.call(parsed)
+        if not can_fail and ret_val != 0:
+            raise Exception("command returned "+str(ret_val))
+        return ret_val == 0
+
+
+# more low level interface returning return value, stdout and stderr
+# modified from https://stackoverflow.com/questions/30937829
+def execute(command, shell=False):
+    with TemporaryFile() as t:
+        try:
+            parsed = parse_command(command, shell)
+            out = subprocess.check_output(parsed, stderr=t).strip()
+            t.seek(0)
+            return {"ret": 0, "stdout": out, "stderr": t.read().strip()}
+        except subprocess.CalledProcessError as e:
+            t.seek(0)
+            return {"ret": e.returncode, "stdout": None, "stderr": t.read().strip()}
 
 def parse_command(command, shell):
     command_list = parse_into_arguments(command, shell)
     return map(lambda arg: arg.encode('ascii', 'replace'), command_list)
+
 
 def parse_into_arguments(command, shell):
     if isinstance(command, str) or isinstance(command, unicode):
@@ -57,9 +75,11 @@ def parse_into_arguments(command, shell):
     else:
         raise Exception("Unsupported type issued to command")
 
+
 def load_conf(path):
     with open(path, 'r') as data_file:
         return json.load(data_file)
+
 
 def save_conf(path, data):
     with open(path, 'w') as data_file:
