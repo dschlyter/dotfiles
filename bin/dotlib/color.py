a0518d18749b5cd1424276d9c9a407eea1c74f0b
David Schlyter <dschlyter@gmail.com>
2025-06-24 02:37:50 +0200

    h graph and ffcut filter_complex

--- bin/dotlib/color.py
+++ bin/dotlib/color.py
@@ -5,6 +5,15 @@ YELLOW = "\033[33m"
 BLUE = "\033[34m"
 MAGENTA = "\033[35m"
 CYAN = "\033[36m"
+
+RED_BG = "\033[41m"
+GREEN_BG = "\033[42m"
+YELLOW_BG = "\033[43m"
+BLUE_BG = "\033[44m"
+MAGENTA_BG = "\033[45m"
+CYAN_BG = "\033[46m"
+WHITE_BG = "\033[47m"
+
 RESET = "\033[0m"
 
 
@@ -34,3 +43,24 @@ def cyan(str):
 
 def nothing(str):
     return str
+
+def red_bg(str):
+    return f"{RED_BG}{str}{RESET}"
+
+def green_bg(str):
+    return f"{GREEN_BG}{str}{RESET}"
+
+def yellow_bg(str):
+    return f"{YELLOW_BG}{str}{RESET}"
+
+def blue_bg(str):
+    return f"{BLUE_BG}{str}{RESET}"
+
+def magenta_bg(str):
+    return f"{MAGENTA_BG}{str}{RESET}"
+
+def cyan_bg(str):
+    return f"{CYAN_BG}{str}{RESET}"
+
+def white_bg(str):
+    return f"{WHITE_BG}{str}{RESET}"
