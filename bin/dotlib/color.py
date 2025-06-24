b317742fd6cdac99725bf84d26e6db14ab4c7fc5
David Schlyter <dschlyter@gmail.com>
2025-06-24 18:11:29 +0200

    h color update

--- bin/dotlib/color.py
+++ bin/dotlib/color.py
@@ -1,4 +1,5 @@
 # Define ANSI escape codes for colors
+
 RED = "\033[31m"
 GREEN = "\033[32m"
 YELLOW = "\033[33m"
@@ -17,50 +18,95 @@ WHITE_BG = "\033[47m"
 RESET = "\033[0m"
 
 
-def red(str):
-    return f"{RED}{str}{RESET}"
+def red(text):
+    return f"{RED}{text}{RESET}"
 
 
-def green(str):
-    return f"{GREEN}{str}{RESET}"
+def green(text):
+    return f"{GREEN}{text}{RESET}"
 
 
-def yellow(str):
-    return f"{YELLOW}{str}{RESET}"
+def yellow(text):
+    return f"{YELLOW}{text}{RESET}"
 
 
-def blue(str):
-    return f"{BLUE}{str}{RESET}"
+def blue(text):
+    return f"{BLUE}{text}{RESET}"
 
 
-def magenta(str):
-    return f"{MAGENTA}{str}{RESET}"
+def magenta(text):
+    return f"{MAGENTA}{text}{RESET}"
 
 
-def cyan(str):
-    return f"{CYAN}{str}{RESET}"
+def cyan(text):
+    return f"{CYAN}{text}{RESET}"
 
 
-def nothing(str):
-    return str
+def nothing(text):
+    return text
 
-def red_bg(str):
-    return f"{RED_BG}{str}{RESET}"
+def red_bg(text):
+    return f"{RED_BG}{text}{RESET}"
 
-def green_bg(str):
-    return f"{GREEN_BG}{str}{RESET}"
+def green_bg(text):
+    return f"{GREEN_BG}{text}{RESET}"
 
-def yellow_bg(str):
-    return f"{YELLOW_BG}{str}{RESET}"
+def yellow_bg(text):
+    return f"{YELLOW_BG}{text}{RESET}"
 
-def blue_bg(str):
-    return f"{BLUE_BG}{str}{RESET}"
+def blue_bg(text):
+    return f"{BLUE_BG}{text}{RESET}"
 
-def magenta_bg(str):
-    return f"{MAGENTA_BG}{str}{RESET}"
+def magenta_bg(text):
+    return f"{MAGENTA_BG}{text}{RESET}"
 
-def cyan_bg(str):
-    return f"{CYAN_BG}{str}{RESET}"
+def cyan_bg(text):
+    return f"{CYAN_BG}{text}{RESET}"
 
-def white_bg(str):
-    return f"{WHITE_BG}{str}{RESET}"
+def white_bg(text):
+    return f"{WHITE_BG}{text}{RESET}"
+
+
+# 256 colors
+
+def color_fg_256(color_code, text):
+    return f"\033[38;5;{color_code}m{text}\033[0m"
+
+def color_bg_256(color_code, text):
+    return f"\033[48;5;{color_code}m{text}\033[0m"
+
+code_muted_blue = 24
+code_muted_green = 22
+code_muted_red = 88
+code_muted_yellow = 136
+code_muted_purple = 54
+
+def muted_blue(text):
+    return color_fg_256(code_muted_blue, text)
+
+def muted_green(text):
+    return color_fg_256(code_muted_green, text)
+
+def muted_red(text):
+    return color_fg_256(code_muted_red, text)
+
+def muted_yellow(text):
+    return color_fg_256(code_muted_yellow, text)
+
+def muted_purple(text):
+    return color_fg_256(code_muted_purple, text)
+
+def muted_blue_bg(text):
+    return color_bg_256(code_muted_blue, text)
+
+def muted_green_bg(text):
+    return color_bg_256(code_muted_green, text)
+
+def muted_red_bg(text):
+    return color_bg_256(code_muted_red, text)
+
+def muted_yellow_bg(text):
+    return color_bg_256(code_muted_yellow, text)
+
+def muted_purple_bg(text):
+    return color_bg_256(code_muted_purple, text)
