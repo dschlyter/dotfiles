ad13fd302482f1d1d77c2fbbe55f0b44b3baba75
David Schlyter <dschlyter@gmail.com>
2025-11-03 22:37:00 +0100

    new computer setup tweaks

--- docker/devplay/build.sh
+++ docker/devplay/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+CONTAINER_CMD="docker"
+if command -v podman; then
+    CONTAINER_CMD="podman"
+fi
+
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
 
-docker build -t devplay .
+"$CONTAINER_CMD" build -t devplay .
