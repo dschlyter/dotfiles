dd9e58307355aac9eaf8175f6fa11782acb0e0d5
David Schlyter <dschlyter@gmail.com>
2026-03-11 16:53:10 +0100

    add "safe" command for ai

---
+++ docker/safe/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONTAINER_CMD="docker"
+if command -v podman; then
+    CONTAINER_CMD="podman"
+fi
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+"$CONTAINER_CMD" build -t safe .
